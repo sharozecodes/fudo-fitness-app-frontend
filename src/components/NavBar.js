e5eaf24a965a1a2cf553ec7eccb6d42b60ace24d
sharozecodes <126905674+sharozecodes@users.noreply.github.com>
2023-10-06 07:09:52 -0400

    adds bootstrapped navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,31 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function NavBar() {
   return (
     <div className="navbar">
-      <NavLink exact to="/" activeClassName="active">
-        Home
-      </NavLink>
-      <NavLink exact to="/login" activeClassName="active">
-        Login
-      </NavLink>
-      <NavLink exact to="/signup" activeClassName="active">
-        Signup
-      </NavLink>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            Fudo - Your Fitness Guide
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/login">
+              Login
+            </Nav.Link>
+            <Nav.Link as={Link} to="/signup">
+              Sign Up
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from "./components/App";
 import Login from "./pages/Login";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import SignUp from "./pages/SignUp";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const router = createBrowserRouter([
   {
