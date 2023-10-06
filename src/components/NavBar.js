5b88ac5b912b3b7690443f25a5d866764034403d
sharozecodes <126905674+sharozecodes@users.noreply.github.com>
2023-10-06 09:18:50 -0400

    removes extra imports

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 function NavBar() {
   return (
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,17 @@
 import React from "react";
 import NavBar from "../components/NavBar";
 import LoginForm from "../components/LoginForm";
+import Button from "react-bootstrap/Button";
 
 function Login() {
   return (
     <div>
       <NavBar />
       <LoginForm />
+      <p>
+        Don't have an account? &nbsp;
+        <Button color="secondary">Sign Up</Button>
+      </p>
     </div>
   );
 }
