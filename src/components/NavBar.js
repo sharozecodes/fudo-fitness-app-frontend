3fb143e0b3aa8b6e42ba75b70ff2758ac5ef8f65
sharozecodes <126905674+sharozecodes@users.noreply.github.com>
2023-10-07 16:57:17 -0400

    removes login and home routes from navbars

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -22,12 +22,6 @@ function NavBar() {
             Fudo - Your Fitness Guide
           </Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">
-              Home
-            </Nav.Link>
-            <Nav.Link as={Link} to="/login">
-              Login
-            </Nav.Link>
             <Nav.Link as={Link} to="/workouts">
               Workouts
             </Nav.Link>
