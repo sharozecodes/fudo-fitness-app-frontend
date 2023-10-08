fa1771bfd3dd7cc083e9140a0ed05af9cc693c3b
sharozecodes <126905674+sharozecodes@users.noreply.github.com>
2023-10-08 12:07:52 -0400

    adds logo to navBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,6 +5,7 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
+import Image from "react-bootstrap/Image";
 
 function NavBar({ loggedIn, setLoggedIn, user }) {
   const handleLogout = () => {
@@ -28,8 +29,15 @@ function NavBar({ loggedIn, setLoggedIn, user }) {
     <div className="navbar">
       <Navbar bg="dark" data-bs-theme="dark" className="navbar-custom">
         <Container fluid>
-          <Navbar.Brand as={Link} to="/">
-            Fudo - Your Fitness Guide
+          <Navbar.Brand as={Link} to="/" className="d-flex align-items-center">
+            <Image
+              src="https://cdn.pixabay.com/photo/2017/01/31/23/42/animal-2028258_1280.png"
+              alt="Logo"
+              className="navbar-logo"
+            />
+            <div className="d-flex flex-column align-items-center brand-text">
+              FUDO
+            </div>
           </Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link as={Link} to="/workouts">
