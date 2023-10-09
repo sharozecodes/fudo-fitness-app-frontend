cf1fc29142f4a6129a6dd02db640c5650d328f8d
sharozecodes <126905674+sharozecodes@users.noreply.github.com>
2023-10-09 17:10:16 -0400

    updates user profile page with css and buttons

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function UserProfile({ user }) {
-  return user ? <h2>HELLLOOOOOO {user.name}</h2> : <></>;
+  return user ? (
+    <div className="centered-container">
+      <h1>
+        Welcome, <stong>{user.name}</stong>!
+      </h1>{" "}
+      <h3 style={{ marginBottom: "1em" }}>What would you want to do today?</h3>
+      <div>
+        <Button variant="secondary" style={{ marginRight: "1em" }}>
+          Update Profile
+        </Button>
+        <Button variant="danger">Delete Profile</Button>{" "}
+      </div>
+    </div>
+  ) : (
+    <></>
+  );
 }
 
 export default UserProfile;
